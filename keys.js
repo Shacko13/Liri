c68c55bb1eb42c8a911983c14fa04b4d3abdd3e9
Todd Szakacs <tszakacs@msn.com>
2017-10-04 23:21:52 -0400

    liri.js updates

--- keys.js
+++ keys.js
@@ -1,18 +1,12 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'nz9ekvE8WIzSQzwP77MEl2tS8',
-  consumer_secret: '1Y273l8hFMV3ZFGsGlSd8i9Rr3T1MEvSSjP9woUCL7KVM8cgWe',
+exports.twitterKeys = {
+  consumer_key: 'x9mFM0AuJyGlMiovoqbHTxww0',
+  consumer_secret: 'ntSrAyLWG6sAkWhhv0QlQJkqvz9AXTUPWMHYE55idrDCA2UVPz',
   access_token_key: '914304313798864896-MySTdfnaP3UtTIRNKlEazb2Mel7fjCY',
   access_token_secret: 'vI1XcRIpvEmESu596AtMWZJ2HfB7LJLC2UUli6qzK0gql',
 }
 
-module.exports = twitterKeys;
 
-var spotifyApi = new SpotifApi({
-  clientId : '',
-  clientSecret : '',
-  redirectUri : 'http://www.example.com/callback'
-});
 
-module.exports = SpotifyApi;
+
--- liri.js
+++ liri.js
@@ -1,28 +1,39 @@
-// Grab the keys file
-// Grab the npm packages
+
 var keys = require("./keys.js");
-	//console.log(keys);
-var twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request');
-var fs - require('fs');
-var parameters = process.argv.slice(3);
 
+var Twitter = require('twitter');
 
+var getMyTweets = function() {
 
-var action = process.argv[2];
-	switch(action) {
-	case 'my-tweets' :
-	  myTweets();
-	  break;
-	case 'spotify-this-song' :
-	  myMusic();
-	  break;
-	case 'movie-this' :
-	  myMovie();
-	  break;
-	case 'do-what-it-says' :
-	  doWhat();
-	  break;
+	var client = new Twitter(keys.twitterKeys)
 
-	};
+	var params = {screen_name: 'sshackenstein'};
+
+	// Getting timeline statuses
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			//console.log(tweets);
+			for (var i = 0; i < tweets.length; i++) {
+				console.log(tweets[i].created_at);
+				console.log(' ');
+				console.log(tweets[i].text);
+			}
+		}
+	});	
+}
+
+var pick = function(caseData, functionData) {
+	switch(caseData) {
+		case 'my-tweets' :
+			getMyTweets();
+			break;
+		default;
+		console.log('LIRI does not know that');
+	}
+}
+
+var runThis = function(argOne, argTwo) {
+	pick(argOne, argTwo);
+};
+
+runThis(process.argv[2], process.argv[3]);
